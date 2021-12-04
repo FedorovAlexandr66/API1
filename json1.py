ad18bff61b3a135fdc4c3b2dbc7eb97a8dcccaac
fedorov alexandr <fedorov4433@mail.ru>
2021-12-04 23:19:40 +0500

    этап 2

--- json1.py
+++ json1.py
@@ -3,6 +3,12 @@ from urllib.request import urlopen
 import json
 
 
+def get_top_limb():
+    data = urlopen('https://lenta.ru/rss').read().decode('utf8')
+    root = ET.fromstring(data)
+    return root[0]
+
+
 def get_uncos(ch: ET.Element):
     uncos = []
     for i in ch.findall('item'):
@@ -13,17 +19,21 @@ def get_uncos(ch: ET.Element):
     return uncos
 
 
-def get_top_limb():
-    data = urlopen('https://lenta.ru/rss').read().decode('utf8')
-    root = ET.fromstring(data)
-    return root[0]
+def get_full_uncos(ch: ET.Element):
+    full_uncos = []
+    for i in ch.findall('item'):
+        fields_dict = {}
+        for f in i:
+            fields_dict[f.tag] = f.text
+        full_uncos.append(fields_dict)
+    return full_uncos
 
 
-def keep_json(ch):
-    json_file = json.dumps(get_uncos(ch), ensure_ascii=False).encode('utf8')
-    with open("news.json", 'wb') as f:
+def keep_json(file_name, uncos):
+    json_file = json.dumps(uncos, ensure_ascii=False).encode('utf8')
+    with open(file_name, 'wb') as f:
         f.write(json_file)
 
 
 channel = get_top_limb()
-keep_json(channel)
+keep_json("full_uncos.json", get_full_uncos(channel))
